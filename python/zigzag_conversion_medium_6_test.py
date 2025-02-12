e3a6fa350bfadef57fd8e2752e9992b2ae075da8
Matthew Ault <matthew.ault@gmail.com>
2025-02-12 09:33:23 +0000

    simplify zigzag solution

--- python/zigzag_conversion_medium_6_test.py
+++ python/zigzag_conversion_medium_6_test.py
@@ -3,27 +3,17 @@ import pytest
 
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        list = []
-        for row in range(numRows):
-            list.append("")
+        list = ["" for row in range(numRows)]
         row = 0
-        goingUpwards = False
+        rowDelta = 1
         for c in s:
             list[row] += c
             if row == 0:
-                goingUpwards = False
-                row = row + 1
+                rowDelta = 1
             elif row == numRows - 1:
-                goingUpwards = True
-                row = row - 1
-            elif goingUpwards:
-                row = row - 1
-            elif not goingUpwards:
-                row = row + 1
-        out = ""
-        for sub in list:
-            out += sub
-        return out
+                rowDelta = -1
+            row = row + rowDelta
+        return "".join(list)
 
 
 test_data = [
